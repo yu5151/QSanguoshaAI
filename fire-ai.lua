b0bf5b29c5ccae0683d42093d6f3a2483de50055
mystery333 <391036126@qq.com>
2013-01-28 14:04:15 +0800

    修复太史慈手牌溢出不天义的bug。

--- fire-ai.lua
+++ fire-ai.lua
@@ -402,7 +402,7 @@ sgs.ai_skill_use_func.TianyiCard=function(card,use,self)
 		self:sortByKeepValue(cards,true)
 		for _, enemy in ipairs(self.enemies) do
 			if not (enemy:hasSkill("kongcheng") and enemy:getHandcardNum() == 1) and not enemy:isKongcheng() 
-					and not enemy:hasSkill("tuntian") --and not self:hasLoseHandcardEffective(enemy) 
+					and not enemy:hasSkill("tuntian") and self:hasLoseHandcardEffective(enemy) 
 				then
 				use.card = sgs.Card_Parse("@TianyiCard=" .. cards[1]:getId())
 				if use.to then use.to:append(enemy) end

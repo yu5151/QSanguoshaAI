69050f214ef99281eec8db8cc2fcd55573b33bed
mystery333 <391036126@qq.com>
2013-01-31 22:39:02 +0800

    蒋琬别刷屏。

--- wisdom-ai.lua
+++ wisdom-ai.lua
@@ -251,7 +251,7 @@ sgs.ai_skill_use_func.HouyuanCard = function(card, use, self)
 			use.to:append(target)
 		end
 		use.card = sgs.Card_Parse("@HouyuanCard=" .. table.concat(usecards, "+"))
-		if use.card then
+		if use.to then
 			self.player:speak("有你这样出远门不带粮食的么？接好了！")
 		end
 	end

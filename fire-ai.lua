920a6332f0a147239ad798558dd25e86083fb6fe
mystery333 <391036126@qq.com>
2013-02-14 14:50:40 +0800

    猛进满血狮子。

--- fire-ai.lua
+++ fire-ai.lua
@@ -547,7 +547,7 @@ sgs.ai_chaofeng.yanliangwenchou = 1
 
 sgs.ai_skill_invoke.mengjin = function(self, data)
 	local effect = data:toSlashEffect()
-	if effect.to:getCardCount(true) == 1 and effect.to:hasArmorEffect("SilverLion") then return false end
+	if effect.to:getCardCount(true) == 1 and effect.to:hasArmorEffect("SilverLion") and effect.to:isWounded() then return false end
 	return not self:isFriend(effect.to)
 end
 

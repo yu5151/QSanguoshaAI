d288d4097bad77a66d3944d3949e6c4a0d44a02b
haveatry823 <401068081@qq.com>
2013-03-27 20:35:01 +0800

    尝试解决自己出牌阶段，决斗别人却有杀不出的情况。 issue #88

--- standard_cards-ai.lua
+++ standard_cards-ai.lua
@@ -1478,13 +1478,14 @@ sgs.ai_keep_value.Duel = 1.7
 sgs.dynamic_value.damage_card.Duel = true
 
 sgs.ai_skill_cardask["duel-slash"] = function(self, data, pattern, target)
+	if self.player:getPhase()==sgs.Player_Play then return self:getCardId("Slash") end
+
 	if sgs.ai_skill_cardask.nullfilter(self, data, pattern, target) then return "." end
 	if self.player:hasFlag("will_wake") then return "." end
 	if (target:hasSkill("wuyan") or self.player:hasSkill("wuyan")) and not target:hasSkill("jueqing") then return "." end
 	if self.player:getMark("@fenyong") >0 and self.player:hasSkill("fenyong") and not target:hasSkill("jueqing") then return "." end
 	
-	if self:cantbeHurt(target) then return "." end
-	if self.player:getPhase()==sgs.Player_Play then return self:getCardId("Slash") end
+	if self:cantbeHurt(target) then return "." end	
 	
 	if self:isFriend(target) and target:hasSkill("rende") and self.player:hasSkill("jieming") then return "." end
 	if self:isEnemy(target) and not self:isWeak() and self:getDamagedEffects(self.player, target) then return "." end

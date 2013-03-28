17189e2dc2a5a5c949a52c34391bb3c7f142f1f3
mystery333 <391036126@qq.com>
2013-03-28 21:08:23 +0800

    先吃桃，再血祭或甘露什么的不科学。

--- smart-ai.lua
+++ smart-ai.lua
@@ -456,10 +456,6 @@ function SmartAI:getDynamicUsePriority(card)
 			end
 		end
 
-		if use_card:isKindOf("Peach") and self.player:getHp() == 1 then
-			value = 8
-		end
-
 		if use_card:isKindOf("YanxiaoCard") and self.player:containsTrick("YanxiaoCard") then
 			value = 0.1
 		end

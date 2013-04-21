5658b625c9342f47ba4b8204f1fae78b6870f2f4
mystery333 <391036126@qq.com>
2013-04-21 10:57:31 +0800

    don't use SupplyShortage to JuaoTarget

--- maneuvering-ai.lua
+++ maneuvering-ai.lua
@@ -226,6 +226,7 @@ function SmartAI:useCardSupplyShortage(card, use)
 
 	local getvalue = function(enemy)
 		if enemy:containsTrick("supply_shortage") or enemy:containsTrick("YanxiaoCard") then return -100 end
+		if enemy:getMark("juao") > 0 then return -100 end
 		if enemy:hasSkill("qiaobian") and not enemy:containsTrick("supply_shortage") and not enemy:containsTrick("indulgence") then return -100 end
 		if zhanghe_seat > 0 and (self:playerGetRound(zhanghe) <= self:playerGetRound(enemy) and self:enemiesContainsTrick() <= 1 or not enemy:faceUp()) then
 			return - 100 end

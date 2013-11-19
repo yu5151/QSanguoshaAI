24793211f9d486ce04c79a85358e3a35a835f6b6
lrl026 <mystarcraft@163.com>
2013-11-19 20:14:21 +0800

    漏了

--- standard_cards-ai.lua
+++ standard_cards-ai.lua
@@ -633,7 +633,7 @@ function SmartAI:useCardSlash(card, use)
 					if use.to then use.to = sgs.SPlayerList() end
 					return
 				end
-				if self.player:hasSkill("jilve") and self.player:getMark("@bear") > 0 and self.player:hasFlag("JilveWansha") and target:getHp() == 1
+				if self.player:hasSkill("jilve") and self.player:getMark("@bear") > 0 and not self.player:hasFlag("JilveWansha") and target:getHp() == 1
 					and (target:isKongcheng() or getCardsNum("Jink", target) < 1 or sgs.card_lack[target:objectName()]["Jink"] == 1) then
 					use.card = sgs.Card_Parse("@JilveCard=.")
 					sgs.ai_skill_choice.jilve = "wansha"

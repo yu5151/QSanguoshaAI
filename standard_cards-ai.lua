408d5d105eb601435aa17ea383e98adb36d8941f
lrl026 <mystartcraft@163.com>
2013-12-19 22:55:55 +0800

    isjinkeffected会触发log信息

--- standard_cards-ai.lua
+++ standard_cards-ai.lua
@@ -776,13 +776,14 @@ end
 
 sgs.ai_skill_cardask["slash-jink"] = function(self, data, pattern, target)
 	local function getJink()
-		if target then
+		if target and target:hasSkill("dahe") and self.player:hasFlag("dahe") then
 			for _, card in ipairs(self:getCards("Jink")) do
-				if self.room:isJinkEffected(self.player, card) then
+				if card:getSuit() == sgs.Card_Heart then
 					return card:getId()
 				end
 			end
 		end
+		return nil
 	end
 
 	local slash

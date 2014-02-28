b6f94797938517c1568baad9d7bfde036c4efaad
lrl026 <mystarcraft@163.com>
2014-03-01 00:35:47 +0800

    3月了

--- smart-ai.lua
+++ smart-ai.lua
@@ -10,7 +10,7 @@ math.randomseed(os.time())
 -- SmartAI is the base class for all other specialized AI classes
 SmartAI = class "SmartAI"
 
-version = "QSanguosha AI 20140222 (V1.29 Alpha)"
+version = "QSanguosha AI 20140301 (V1.30 Alpha)"
 
 -- checkout https://github.com/haveatry823/QSanguoshaAI for details
 
